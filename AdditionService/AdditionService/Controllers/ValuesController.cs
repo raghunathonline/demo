91b5b266a57263ab304da58d5404786e224ef5dc
Raghunathk <raghunathk@outlook.com>
2019-07-27 17:36:51 +0530

    initial commit
    initial commit

---
+++ AdditionService/AdditionService/Controllers/ValuesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+namespace AdditionService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+       
+        
+        // GET api/values/5
+        [HttpGet]
+        public ActionResult<double> Get()
+        {
+            var queryString = Request.Query;
+            StringValues param1; StringValues param2;
+            queryString.TryGetValue("p1", out param1);
+            queryString.TryGetValue("p2", out param2);
+            if(Convert.ToDouble(param1)==0 || Convert.ToDouble(param2)==0)
+                return Ok(0);
+            return Ok(Convert.ToDouble(param1) + Convert.ToDouble(param2));
+        }
+
+    }
+}
---
+++ MultiplicationService/MultiplicationService/Controllers/ValuesController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+namespace MultiplicationService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<double>> Get()
+        {
+            var queryString = Request.Query;
+            StringValues param1; StringValues param2;
+            queryString.TryGetValue("p1", out param1);
+            queryString.TryGetValue("p2", out param2);
+            return Ok(Convert.ToDouble(param1) * Convert.ToDouble(param2));
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
